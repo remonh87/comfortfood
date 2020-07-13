4e7f5b4fdf6a2c3b68a4a5116f1300817381fc04
Rémon <remonhelmond@gmail.com>
2020-07-13 17:26:54 +0200

    improve error messages

--- app/src/main/java/com/remonh87/comfortfood/MethodChannelHandler.kt
+++ app/src/main/java/com/remonh87/comfortfood/MethodChannelHandler.kt
@@ -3,7 +3,7 @@ package com.remonh87.comfortfood
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
-class MethodChannelHandler(
+class OrderMethodCallHandler(
     val addOrder: (line: OrderLine) -> Unit
 ) : MethodChannel.MethodCallHandler {
 
@@ -23,7 +23,7 @@ class MethodChannelHandler(
             } else {
                 result.error(
                     fatalFailureCode, "Incorrect arguments supplied",
-                    "Should be a double"
+                    "Description should be a string and price should be a double"
                 )
             }
         } else {
